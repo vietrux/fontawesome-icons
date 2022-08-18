6d2e87addb968be30cc0c7928e45c5971525434b
unknown <leviettrung.social@gmail.com>
2022-08-18 17:10:46 +0700

    big update

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,150 @@
 import { useEffect, useState, useTransition } from 'react';
-//import category from './category.json';
+import category from './category.json';
 import IconModel from './IconModel';
 import viconslist from './vicons.json';
 
 //conver object to array viconslist
 const vicons = Object.keys(viconslist).map(key => viconslist[key as keyof typeof viconslist]);
+const vcategory = Object.keys(category).map(key => category[key as keyof typeof category]);
+const vstyle = ["solid", "regular", "duotone", "brands", "light", "thin"];
 
-export default function Example() {
+export default function App() {
+  const [input, setInput] = useState('')
   const [query, setQuery] = useState('')
-  const [results, setResults] = useState(vicons.slice(0, 100))
+  const [results, setResults] = useState([] as any[])
   const [isPending, startTransition] = useTransition()
+  const [listfilter, setListfilter] = useState([] as string[])
+  const [liststyle, setListstyle] = useState([] as string[])
+  const [listiconfilter, setListiconfilter] = useState([] as any[])
   useEffect(() => {
-  query === ("" || undefined)
-    ? setResults(vicons.slice(0, 100))
-    :
-    startTransition(() => {
-      setResults(vicons.filter((icon: any) => {
-        return icon.label.toLowerCase().includes(query.toLowerCase())
-      }))
+    query === ("") && listiconfilter.length === 0
+      ? setResults(vicons.slice(0, 25))
+      :
+      listiconfilter.length !== 0 ?
+        setResults(vicons.filter((icon: any) => {
+          return icon.label.toLowerCase().includes(query.toLowerCase()) && listiconfilter.includes(icon.iconname)
+        }))
+        :
+        setResults(vicons.filter((icon: any) => {
+          return icon.label.toLowerCase().includes(query.toLowerCase())
+        }))
+
+
+  }, [query, listiconfilter, liststyle])
+
+  useEffect(() => {
+    const listicon = [] as any[]
+    listfilter.forEach((element: string) => {
+      listicon.push(...vcategory.find((category: any) => category.categoryname === element)?.icons as any[])
     })
-  }, [query])
+    if (listicon.length !== 0) {
+      liststyle.forEach((style: string) => {
+        vicons.filter((icon: any) => icon.styles.includes(style)).forEach((icon: any) => {
+        })
+      })
+    }
+    setListiconfilter(listicon)
+  }
+    , [listfilter, liststyle])
+
 
 
   return (
     <>
-      <div className='p-8 sm:p-16'>
+      <div className='p-8 sm:py-16 lg:p-16 '>
         <h1 className='text-[36px] font-bold'>Vicons</h1>
-        <div className='flex flex-col sm:flex-row justify-between items-center'>
-          <div>
-            <label className='block text-gray-700 text-sm font-bold mb-2'>
-              Search
-            </label>
-            <input className="border border-gray-400 rounded-lg p-2 mb-2 "
-              type="text" value={query}
-              onChange={(e) => {
-                setQuery(e.target.value.trim())
-              }} />
-          </div>
-          <h1>Click on a vicon to copy it to your clipboard</h1>
-        </div>
-        {isPending ? <>Loading...</> : 
-         !isPending && results.length !== 0 ? 
-        <ul className="grid grid-cols-3 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-10 gap-4">
+        <div className='flex'>
+          <div className='w-1/5 hidden sm:flex'>
+            <div className='flex flex-col'>
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                  <div className="w-full border-t border-gray-300" />
+                </div>
+                <div className="relative flex justify-center">
+                  <span className="px-3 bg-white text-lg font-medium text-gray-900">Styles</span>
+                </div>
+              </div>
+              {vstyle.map((type: string) => (
+                <div key={type}>
+                  <input id={type} type={'checkbox'} onChange={(e) => {
+                    const { checked } = e.target
+                    if (checked) {
+                      setListstyle([e.target.value, ...liststyle])
 
-          { results.map((icon: any) => (
-            <li key={icon.iconname}>
-              <IconModel icon={icon} />
-            </li>
-          ))
-            }
-        </ul>
-        :'Không tìm thấy kết quả nào'}
+                    } else {
+                      setListstyle(liststyle.filter((item: string) => item !== e.target.value))
+
+                    }
+                  }
+                  } value={type} />
+                  <label htmlFor={type} className='text-sm ml-2'>{type.toUpperCase()}</label>
+                </div>
+              ))}
+              <div className="relative">
+                <div className="absolute inset-0 flex items-center" aria-hidden="true">
+                  <div className="w-full border-t border-gray-300" />
+                </div>
+                <div className="relative flex justify-center">
+                  <span className="px-3 bg-white text-lg font-medium text-gray-900">Category</span>
+                </div>
+              </div>
+              {vcategory.map((category: any) => (
+                <div key={category.label}>
+                  <input id={category.categoryname} type={'checkbox'} onChange={(e) => {
+                    const { checked } = e.target
+                    if (checked) {
+                      setListfilter([e.target.value, ...listfilter])
+
+                    } else {
+                      setListfilter(listfilter.filter((item: string) => item !== e.target.value))
+
+                    }
+                  }
+                  } value={category.categoryname} />
+                  <label htmlFor={category.categoryname} className='text-sm ml-2'>{category.label}</label>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div>
+            <div className='flex flex-col sm:flex-row justify-between items-center'>
+              <div>
+                <label className='block text-gray-700 text-sm font-bold mb-2'>
+                  Search
+                </label>
+                <input className="border border-gray-400 rounded-lg p-2 mb-2 "
+                  type="text" value={input}
+                  onChange={(e) => {
+                    setInput(e.target.value.trim())
+                    startTransition(() => {
+                      setQuery(e.target.value.trim())
+                    })
+                  }} />
+              </div>
+              <h1>Click on a vicon to copy it to your clipboard</h1>
+            </div>
+            {isPending ? <>Loading...</> :
+              !isPending && results.length !== 0 ?
+                <ul className="grid grid-cols-3 sm:grid-cols-5 md:grid-cols-6 lg:grid-cols-8 gap-4">
+
+                  {results.map((icon: any) => (
+                    liststyle.length === 0 ? icon.styles.map((style: any) => (
+                      <li key={icon.iconname + style}>
+                        <IconModel icon={icon} style={style} />
+                      </li>
+                    ))
+                      :
+                      
+                      liststyle.map((style: string) => (
+                        icon.styles.includes(style) &&
+                        <li key={icon.iconname + style}>
+                          <IconModel icon={icon} style={style} />
+                        </li>
+                      ))))}
+                </ul>
+                : 'Không tìm thấy kết quả nào'}
+          </div>
+        </div>
       </div>
     </>
   )
--- src/IconModel.tsx
+++ src/IconModel.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 type IconModelProps = {
+  style?: string;
   icon?: any;
 }
 
@@ -16,13 +17,27 @@ export default function IconModel(props: IconModelProps) {
           setIsOpen(!isOpen);
         }}
         className="w-[6rem] h-[6rem] m-[4px] flex flex-col items-center justify-center border border-slate-200 rounded-[30%] cursor-pointer">
-        {
+        {!props.style ?
           props.icon ? (
-            <svg className="w-8 h-8 fill-slate-600" viewBox={props.icon.svg[props.icon.styles[0] as keyof typeof props.icon.styles].viewBox}>
-              <path d={props.icon.svg[props.icon.styles[0] as keyof typeof props.icon.styles].path} />
+            <svg className="w-8 h-8" viewBox={props.icon.svg[props.icon.styles[0] as keyof typeof props.icon.styles].viewBox}>
+              <path className="fill-slate-600" d={props.icon.svg[props.icon.styles[0] as keyof typeof props.icon.styles].path} />
             </svg>
           )
             : null
+          : props.style && props.style === "duotone" ?
+            (
+              <>
+                <svg className="w-8 h-8" viewBox={props.icon.svg[props.style].viewBox}>
+                  <path className="fill-slate-400" d={props.icon.svg[props.style].path[0]} />
+                  <path className="fill-slate-800" d={props.icon.svg[props.style].path[1]} />
+                </svg>
+              </>
+            ) : 
+            (
+              <svg className="w-8 h-8" viewBox={props.icon.svg[props.style].viewBox}>
+                <path className="fill-slate-600" d={props.icon.svg[props.style].path} />
+              </svg>
+            ) 
         }
         <div
           style={
@@ -35,33 +50,53 @@ export default function IconModel(props: IconModelProps) {
               whiteSpace: "pre-line"
             }
           }
-          className="text-center text-sm ">{props.icon.label}</div>
+          className="text-center text-sm ">{props.icon.iconname}</div>
       </div>
       {isOpen ? (
         <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="fixed inset-0 w-full h-full bg-black opacity-40" onClick={() => setIsOpen(false)}></div>
+          <div className="fixed inset-0 w-full h-full bg-black opacity-40" onClick={() => {
+            setIsOpen(false)
+            setSignal("")
+          }
+          }></div>
           <div className="flex items-center min-h-screen px-4 py-8">
             <div className="relative w-full max-w-lg p-4 mx-auto bg-white rounded-md shadow-lg">
-              <h1 className="font-bold text-xl">{props.icon.label}</h1>
-              <div className="mt-3 flex flex-wrap">
-                {props.icon.styles.map((style: string, index:number) => (
+              <div className="flex items-center flex-wrap">
+                <h1 className="font-bold text-xl">{props.icon.iconname}</h1>
+                {
+                  props.icon.changes.map((change: string) => (
+                    <span key={change} className="ml-2 border border-blue-200 px-2 rounded-lg">{change}</span>
+                  ))
+                }
+              </div>
+              <div className="mt-3 flex flex-wrap justify-around sm:justify-between">
+                {props.icon.styles.map((style: string, index: number) => (
                   <div
+                    key={props.icon.iconname + props.icon.styles[index]}
                     onClick={() => {
                       navigator.clipboard.writeText(props.icon.svg[props.icon.styles[index] as keyof typeof props.icon.styles].raw);
                       setSignal("");
                       setTimeout(() => {
                         setSignal("Copied");
-                      }, 300);
-                      setTimeout(() => {
-                        setSignal("");
-                      } , 1000);
+                      }, 100);
                     }}
                     className="w-[6rem] h-[6rem] m-[4px] flex flex-col items-center justify-center border border-slate-200 rounded-[30%] cursor-pointer">
                     {
                       props.icon ? (
-                        <svg className="w-8 h-8 fill-slate-600" viewBox={props.icon.svg[props.icon.styles[index] as keyof typeof props.icon.styles].viewBox}>
-                          <path d={props.icon.svg[props.icon.styles[index] as keyof typeof props.icon.styles].path} />
+                        <svg className="w-8 h-8" viewBox={props.icon.svg[props.icon.styles[index] as keyof typeof props.icon.styles].viewBox}>
+                          {
+                            props.icon.styles[index] === "duotone" ? (
+                              <>
+                                <path className="fill-slate-400" d={props.icon.svg[props.icon.styles[index] as keyof typeof props.icon.styles].path[0]} />
+                                <path className="fill-slate-800" d={props.icon.svg[props.icon.styles[index] as keyof typeof props.icon.styles].path[1]} />
+                              </>
+                            ) : (
+                              <path className="fill-slate-600" d={props.icon.svg[props.icon.styles[index] as keyof typeof props.icon.styles].path} />
+                            )
+                          }
                         </svg>
+                        // <div
+                        //   dangerouslySetInnerHTML={{ __html: props.icon.svg[props.icon.styles[index] as keyof typeof props.icon.styles].raw }} />
                       )
                         : null
                     }
@@ -83,7 +118,10 @@ export default function IconModel(props: IconModelProps) {
               {signal}
               <div className="items-center gap-2 mt-3 sm:flex">
                 <button className="w-full mt-2 p-2.5 flex-1 text-white bg-indigo-600 rounded-md outline-none ring-offset-2 ring-indigo-600 focus:ring-2"
-                  onClick={() => setIsOpen(false)}
+                  onClick={() => {
+                    setIsOpen(false)
+                    setSignal("")
+                  }}
                 >
                   Close
                 </button>
